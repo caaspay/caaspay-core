7da9962623cf7d49530cbfcdb90376a64d26f4a6
vnealv <neal.alwani@gmail.com>
2025-01-18 16:59:30 +0000

    add testing changes, and switch back to redis main
    Signed-off-by: vnealv <neal.alwani@gmail.com>

--- service/control/authentication/login/lib/Control/Authentication/Login.pm
+++ service/control/authentication/login/lib/Control/Authentication/Login.pm
@@ -4,12 +4,30 @@ use Myriad::Service;
 
 
 async method startup() {
+    my $s = $api->service_by_name('test.service');
+    try {
+#        await $s->call_rpc('request', test_arg1 => 'ffff', test_arg2 => 'gggg');
+    } catch ($err) {
+        $log->warnf('RRR %s', $err);
+    }
     $log->warnf('Started');
 }
 
-async method test_rpc : RPC (%args) {
+async method login : RPC (%args) {
     $log->warnf('Method TEST_RPC: %s', \%args);
-    return { success => 1, name => 'Kyle' };
+    return { success => 1, name => \%args };
+}
+
+
+async method test: Batch()(){
+    await $self->loop->delay_future(after => 60);
+    my $s = $api->service_by_name('control.authentication.login');
+    try {
+        my $f = await $s->call_rpc('login', name => 'TTT', age => 4);
+        $log->warnf('FFFFF %s', $f);
+    } catch ($err) {
+    }
+    return [];
 }
 
 1;
